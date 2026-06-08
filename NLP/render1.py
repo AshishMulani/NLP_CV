cfa83f37b70ab6c00d4085c26d3778af814b1532
pranitalad05 <mulaniashish16@gmail.com>
2026-06-08 07:52:35 +0530

    new

---
+++ NLP/render1.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_admin():
+    return '''<html>
+    <title>My Web Page</title>
+    <body>
+    <h1> <center> Welcome to Flask </center></h1>
+    <h2> <center> The language of Web </center></h2>
+    </body>
+    </html>'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
